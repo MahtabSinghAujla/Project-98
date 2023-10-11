8f57ce6bc00ca88a0fb0301c566ef4ff1b273a82
MahtabSinghAujla <125728731+MahtabSinghAujla@users.noreply.github.com>
2023-10-11 19:15:22 +1100

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,13 @@
+def swapFileData () :
+    filename1=input('Give the first file name: ')
+    filename2=input('Give the second file name: ')
+    with open(filename1,'r') as a :
+        data_a=a.read()
+    with open(filename2, 'r') as b :
+        data_b=b.read()
+    with open(filename1,'w') as a :
+        a.write(data_b)
+    with open(filename2, 'w') as b :
+        b.write(data_a)
+    print('Data swapped')
+swapFileData() 
